df9afc25fcc72afc8c0d6e7f5332b61bd19b3b09
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-07-03 16:36:13 +0200

    Adding git and which-key functionality

---
+++ lua/plugins/git.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        "f-person/git-blame.nvim",
+        config = function()
+            require('gitblame').setup()
+        end,
+    },
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",         -- required
+            "sindrets/diffview.nvim",        -- optional - Diff integration
+            "nvim-telescope/telescope.nvim", -- optional
+        },
+        config = true
+    }
+}
