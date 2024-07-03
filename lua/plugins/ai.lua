bc797fe6c45e03c383e85423817a379646ca6079
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-07-03 16:40:55 +0200

    Adding AI plugins

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        "Exafunction/codeium.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "hrsh7th/nvim-cmp",
+        },
+        config = function()
+            require("codeium").setup({
+            })
+        end
+    },
+    {
+        "https://github.com/github/copilot.vim",
+        cmd = "Copilot"
+    }
+}
