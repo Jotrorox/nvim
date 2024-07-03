971ab34961699ac837f672880e84ff78139061c7
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-07-03 17:15:27 +0200

    Reenabling the csharp support

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("config.lazy")
 require("config.sets")
 require("config.lsp")
+require("config.languages")
--- lua/plugins/csharp.lua
+++ lua/plugins/csharp.lua
@@ -1,14 +1,10 @@
 return {
---   {
-----       "iabdelkareem/csharp.nvim",
-----       dependencies = {
-----           "williamboman/mason.nvim", -- Required, automatically installs omnisharp
-----           "mfussenegger/nvim-dap",
-----           "Tastyep/structlog.nvim", -- Optional, but highly recommended for debugging
-----       },
-----       config = function ()
-----           require("mason").setup() -- Mason setup must run before csharp
-----           require("csharp").setup()
-----       end
-----   }
+    {
+        "iabdelkareem/csharp.nvim",
+        dependencies = {
+            "williamboman/mason.nvim", -- Required, automatically installs omnisharp
+            "mfussenegger/nvim-dap",
+            "Tastyep/structlog.nvim", -- Optional, but highly recommended for debugging
+        },
+    }
 }
