c39841a9f08f5280e04d5e695af782d54fdbe8ad
Johannes Müller <johannes@home.fritz.box>
2024-08-16 00:33:38 +0200

    Making the default theme white and adding neorg

--- lua/plugins/general/theme.lua
+++ lua/plugins/general/theme.lua
@@ -5,7 +5,7 @@ return {
 		priority = 1000, 
         config = function()
             require("catppuccin").setup({
-                flavour = "mocha", -- latte, frappe, macchiato, mocha
+                flavour = "latte", -- latte, frappe, macchiato, mocha
                 transparent_background = false, -- disables setting the background color.
             })
             vim.cmd.colorscheme("catppuccin")
