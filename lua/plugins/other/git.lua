3e800daee9db45f5d80d4c3a6cceebc3452edeee
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-09-17 10:22:30 +0200

    Adding formatting git.lua and adding lazygit support

--- lua/plugins/other/git.lua
+++ lua/plugins/other/git.lua
@@ -1,22 +1,38 @@
 return {
-    {
-        "f-person/git-blame.nvim",
-        config = function()
-            require('gitblame').setup()
-        end,
+  {
+    "f-person/git-blame.nvim",
+    config = function()
+      require('gitblame').setup()
+    end,
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+      "nvim-telescope/telescope.nvim", -- optional
     },
-    {
-        "NeogitOrg/neogit",
-        dependencies = {
-            "nvim-lua/plenary.nvim",         -- required
-            "sindrets/diffview.nvim",        -- optional - Diff integration
-            "nvim-telescope/telescope.nvim", -- optional
-        },
-        config = true,
-        config = function()
-            local neogit = require('neogit')
-            neogit.setup({})
-            vim.keymap.set('n', '<leader>gN', "<cmd>Neogit<cr>", {})
-        end,
+    config = true,
+    config = function()
+      local neogit = require('neogit')
+      neogit.setup({})
+      vim.keymap.set('n', '<leader>gN', "<cmd>Neogit<cr>", {})
+    end,
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>gl", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
+  }
 }
