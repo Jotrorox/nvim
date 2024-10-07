318c3fef2539533712d0a0c5917ada0ef60d5459
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-10-07 14:18:32 +0200

    Disabling codeium.nvim

--- lua/plugins/other/ai.lua
+++ lua/plugins/other/ai.lua
@@ -1,14 +1,14 @@
 return {
-    {
-        "Exafunction/codeium.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "hrsh7th/nvim-cmp",
-        },
-        config = function()
-            require("codeium").setup({})
-        end
-    },
+    -- {
+    --     "Exafunction/codeium.nvim",
+    --     dependencies = {
+    --         "nvim-lua/plenary.nvim",
+    --         "hrsh7th/nvim-cmp",
+    --     },
+    --     config = function()
+    --         require("codeium").setup({})
+    --     end
+    -- },
     {
         "https://github.com/github/copilot.vim",
         cmd = "Copilot"
