d2818558712985b97f76b63091931d0331759ed9
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-10-07 14:33:29 +0200

    Updating the formatting

--- lua/plugins/general/keys.lua
+++ lua/plugins/general/keys.lua
@@ -1,13 +1,13 @@
 return {
-    {
-        "folke/which-key.nvim",
-		priority = 999, 
-        init = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-        end,
-        opts = {
+  {
+    "folke/which-key.nvim",
+    priority = 999, 
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
 
-        },
-    }
+    },
+  }
 }
--- lua/plugins/other/git.lua
+++ lua/plugins/other/git.lua
@@ -8,9 +8,9 @@ return {
   {
     "NeogitOrg/neogit",
     dependencies = {
-      "nvim-lua/plenary.nvim",         -- required
-      "sindrets/diffview.nvim",        -- optional - Diff integration
-      "nvim-telescope/telescope.nvim", -- optional
+      "nvim-lua/plenary.nvim",        
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
     },
     config = true,
     config = function()
