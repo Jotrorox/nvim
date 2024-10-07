1b6509289eaf26e0bc1a5b1472075cbfdd9cf7ec
Johannes Müller <johannes@fedora.fritz.box>
2024-10-07 22:19:09 +0200

    Changing to nordtheme

--- lua/plugins/general/theme.lua
+++ lua/plugins/general/theme.lua
@@ -1,33 +1,33 @@
 return {
-  { 
-    "catppuccin/nvim", 
-    name = "catppuccin", 
-    priority = 1000, 
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha", -- latte, frappe, macchiato, mocha
-        transparent_background = false, -- disables setting the background color.
-      })
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  }
+  -- { 
+  --   "catppuccin/nvim", 
+  --   name = "catppuccin", 
+  --   priority = 1000, 
+  --   config = function()
+  --     require("catppuccin").setup({
+  --       flavour = "mocha", -- latte, frappe, macchiato, mocha
+  --       transparent_background = false, -- disables setting the background color.
+  --     })
+  --     vim.cmd.colorscheme("catppuccin")
+  --   end,
+  -- }
   --    { 
   --        "ellisonleao/gruvbox.nvim", 
   --        priority = 1000 , 
   --        config = true, 
   --        opts = ...
   --    }
-  --  {
-  --    "gbprod/nord.nvim",
-  --    lazy = false,
-  --    priority = 1000,
-  --    config = function()
-  --      require("nord").setup({
-  --        transparent = true
-  --      })
-  --      vim.cmd.colorscheme("nord")
-  --    end,
-  --  },
+  {
+    "gbprod/nord.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("nord").setup({
+        transparent = true
+      })
+      vim.cmd.colorscheme("nord")
+    end,
+  },
   -- {
   --   "ntk148v/komau.vim",
   --   lazy = false,
