fef135a9c8dddbf3d14139b3433f3e66eb83599c
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-11-04 18:17:34 +0100

    Fix the config

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,8 +1,8 @@
--- return {
---   {
---     "supermaven-inc/supermaven-nvim",
---     config = function()
---       require("supermaven-nvim").setup({})
---     end,
---   },
--- }
+return {
+  --   {
+  --     "supermaven-inc/supermaven-nvim",
+  --     config = function()
+  --       require("supermaven-nvim").setup({})
+  --     end,
+  --   },
+}
