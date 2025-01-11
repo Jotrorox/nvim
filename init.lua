9ffc15f393db7161b475caa0597e797efa0ab845
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2025-01-11 13:12:21 +0100

    Update

--- init.lua
+++ init.lua
@@ -1,11 +1,1 @@
-local load = function(mod)
-  package.loaded[mod] = nil
-  require(mod)
-end
-
-load('jotrorox.settings')
-load('jotrorox.commands')
-load('jotrorox.keymaps')
-require('jotrorox.plugins')
-
-pcall(vim.cmd.colorscheme, 'gruvbox')
+require("core_init")
