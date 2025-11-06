13bbf68f101faee705d1647f03a9f853552aad08
Johannes (jotrorox) Müller <mail@jotrorox.com>
2025-11-06 11:53:05 +0100

    Quickfix, misconfigured

--- init.lua
+++ init.lua
@@ -533,21 +533,6 @@ require("lazy").setup({
 		event = "InsertEnter",
 		config = function()
 			require("copilot").setup({
-				panel = {
-					enabled = true,
-					auto_refresh = false,
-					keymap = {
-						jump_prev = "[[",
-						jump_next = "]]",
-						accept = "<CR>",
-						refresh = "gr",
-						open = "<M-CR>",
-					},
-					layout = {
-						position = "bottom", -- | top | left | right | bottom |
-						ratio = 0.4,
-					},
-				},
 				suggestion = {
 					enabled = true,
 					auto_trigger = true,
@@ -563,44 +548,6 @@ require("lazy").setup({
 						dismiss = "<C-]>",
 					},
 				},
-				nes = {
-					enabled = false, -- requires copilot-lsp as a dependency
-					auto_trigger = false,
-					keymap = {
-						accept_and_goto = false,
-						accept = false,
-						dismiss = false,
-					},
-				},
-				auth_provider_url = nil, -- URL to authentication provider, if not "https://github.com/"
-				logger = {
-					file = vim.fn.stdpath("log") .. "/copilot-lua.log",
-					file_log_level = vim.log.levels.OFF,
-					print_log_level = vim.log.levels.WARN,
-					trace_lsp = "off", -- "off" | "messages" | "verbose"
-					trace_lsp_progress = false,
-					log_lsp_messages = false,
-				},
-				copilot_node_command = "node", -- Node.js version must be > 22
-				workspace_folders = {},
-				copilot_model = "",
-				disable_limit_reached_message = false, -- Set to `true` to suppress completion limit reached popup
-				root_dir = function()
-					return vim.fs.dirname(vim.fs.find(".git", { upward = true })[1])
-				end,
-				should_attach = function(_, _)
-					if not vim.bo.buflisted then
-						logger.debug("not attaching, buffer is not 'buflisted'")
-						return false
-					end
-
-					if vim.bo.buftype ~= "" then
-						logger.debug("not attaching, buffer 'buftype' is " .. vim.bo.buftype)
-						return false
-					end
-
-					return true
-				end,
 			})
 		end,
 	},
