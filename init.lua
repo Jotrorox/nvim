6cdb5c335502f3d41726c7fd574e72149aaea3e6
jotrorox <mail@jotrorox.com>
2026-04-12 13:46:08 +0200

    New simpler config

--- init.lua
+++ init.lua
@@ -1,21 +1,123 @@
---[[
-  File: init.lua
-  Description: Entry point file for neovim
-]]
+vim.g.mapleader = " "
 
-vim.g.mapleader = ' '                                                                 -- Use Space, like key for alternative hotkeys
+-- Editor defaults
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.updatetime = 200
+vim.opt.mouse = "a"
 
--- Bootsraping plugin manager
-require "lazy-bootstrap"
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.smartindent = true
 
--- Settings
-require "settings"
+vim.opt.wrap = false
+vim.opt.cursorline = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.undofile = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.completeopt = { "menuone", "noselect" }
 
--- Plugin management {{{
-local lazy = require("lazy")
-lazy.setup("plugins")
--- }}}
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-require "keyboard"
+require("lazy").setup({
+  -- Nord theme
+  {
+    "shaunsingh/nord.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("nord")
+    end,
+  },
 
--- vim:tabstop=2 shiftwidth=2 expandtab syntax=lua foldmethod=marker foldlevelstart=0 foldlevel=0
+  -- Fuzzy finder
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+    },
+  },
+
+  -- LSP
+  {
+    "neovim/nvim-lspconfig",
+  },
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "clangd", "cmake" },
+        automatic_enable = false,
+      })
+
+      vim.lsp.config("clangd", {
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--completion-style=detailed",
+        },
+      })
+
+      vim.lsp.config("cmake", {})
+
+      vim.lsp.enable("clangd")
+      vim.lsp.enable("cmake")
+    end,
+  },
+
+  -- CMake helper
+  {
+    "Civitasv/cmake-tools.nvim",
+    ft = { "c", "cpp", "cmake" },
+    config = function()
+      require("cmake-tools").setup({})
+    end,
+  },
+})
+
+-- LSP keymaps
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local opts = { buffer = ev.buf, noremap = true, silent = true }
+
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+  end,
+})
--- lua/keyboard.lua
+++
@@ -1,4 +0,0 @@
--- This file is intentionally left empty
--- Put your keybindings with wk package here
-
-local wk = require('which-key')
